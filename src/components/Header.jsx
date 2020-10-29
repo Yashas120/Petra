a77cd4a3a63663db06160ab0ad7b03dd6b31fe62
Vishal R <vishalramesh01@gmail.com>
2020-10-29 10:40:50 +0530

    Finish Header section

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./styles/Header.css";
 import SearchIcon from "@material-ui/icons/Search";
 import LanguageIcon from "@material-ui/icons/Language";
@@ -6,24 +6,83 @@ import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
 import { Avatar } from "@material-ui/core";
 import AddIcon from "@material-ui/icons/Add";
 import SubIcon from "@material-ui/icons/Remove";
+import Clear from "@material-ui/icons/ClearRounded";
+import Hamburger from "@material-ui/icons/MenuRounded";
 import DatePicker from "./DatePicker";
+import axios from "axios";
 
 function Header() {
   const [showDiv, setShowDiv] = useState(false);
+  const [showSearchSuggestions, setShowSearchSuggestions] = useState(false);
   const [adultNumber, setAdultNumber] = useState(0);
   const [childernNumber, setChildernNumber] = useState(0);
   const [infantNumber, setInfantNumber] = useState(0);
   const [petNumber, setPetNumber] = useState(0);
+  const [searchValue, setSearchValue] = useState([
+    "Banaglore, India",
+    "Mysore, India",
+    "Mumbai, India",
+    "Delhi, India",
+    "Chennai, India",
+  ]);
+  const [showProfileDiv, setShowProfileDiv] = useState(false);
+  const [showLangDiv, setShowLangDiv] = useState(false);
+
+  // useEffect(() => {
+  //   // axios
+  //   //   .get(
+  //   //     `Link`
+  //   //   )
+  //   //   .then((response) => {
+  //   //     console.log(response.data);
+  //   //   })
+  //   //   .catch((error) => {
+  //   //     console.log(error);
+  //   //   });
+  //   // Not working
+  // });
 
   return (
     <div className="header">
       <section className="Nav-bar">
         <span className="Header-brand">PeTra</span>
         <div className="header-right">
-          <p>Become a host</p>
-          <LanguageIcon></LanguageIcon>
-          <ExpandMoreIcon></ExpandMoreIcon>
-          <Avatar></Avatar>
+          <div
+            className="language-div"
+            onClick={() => {
+              if (!showLangDiv) setShowLangDiv(true);
+              else setShowLangDiv(false);
+            }}
+          >
+            <div>
+              <LanguageIcon className="globe"></LanguageIcon>
+              <ExpandMoreIcon className="Expand-more"></ExpandMoreIcon>
+            </div>
+          </div>
+          {showLangDiv ? (
+            <div className="lang-div">
+              <LanguageIcon></LanguageIcon>
+              <p>English (IN)</p>
+            </div>
+          ) : null}
+          <div
+            className="profile"
+            onClick={() => {
+              if (!showProfileDiv) setShowProfileDiv(true);
+              else {
+                setShowProfileDiv(false);
+              }
+            }}
+          >
+            <Hamburger className="menu-icon"></Hamburger>
+            <Avatar className="profile-photo"></Avatar>
+          </div>
+          {showProfileDiv ? (
+            <div className="profile-div">
+              <p>Sign Up</p>
+              <p>Log in</p>
+            </div>
+          ) : null}
         </div>
       </section>
       <section className="Search-Bar">
@@ -40,7 +99,20 @@ function Header() {
                 type="text"
                 className="location-input"
                 placeholder="Where are you going?"
+                onChange={() => {
+                  if (document.querySelector(".location-input").value !== "")
+                    setShowSearchSuggestions(true);
+                  else setShowSearchSuggestions(false);
+                }}
               ></input>
+              {showSearchSuggestions ? (
+                <Clear
+                  onClick={() => {
+                    document.querySelector(".location-input").value = "";
+                    setShowSearchSuggestions(false);
+                  }}
+                ></Clear>
+              ) : null}
             </div>
             <div className="Check-in">
               <p>Check In</p>
@@ -68,6 +140,25 @@ function Header() {
             </div>
           </div>
         </form>
+        {showSearchSuggestions ? (
+          <div className="search-suggestions">
+            {searchValue.map((suggestions, index) => {
+              return (
+                <div
+                  className="suggestion-items"
+                  id={index}
+                  onClick={() => {
+                    document.querySelector(
+                      ".location-input"
+                    ).value = suggestions;
+                  }}
+                >
+                  {suggestions}
+                </div>
+              );
+            })}
+          </div>
+        ) : null}
         {showDiv ? (
           <div className="guest-menu">
             <div className="guest-menu-adult">
@@ -195,11 +286,20 @@ function Header() {
       <section className="header-img">
         <img
           src="./mountain.png"
-          alt="mountain-image"
+          alt="mountain-img"
           className="header-image"
           onClick={() => {
             if (showDiv) {
               setShowDiv(false);
+            }
+            if (showSearchSuggestions) {
+              setShowSearchSuggestions(false);
+            }
+            if (showProfileDiv) {
+              setShowProfileDiv(false);
+            }
+            if (showLangDiv) {
+              setShowLangDiv(false);
             }
           }}
         ></img>
